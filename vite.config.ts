95487b56ad114732bb77045ab0ca9b6d54a79643
arm <arm.dev024@gmail.com>
2026-02-09 10:26:13 +0545

    https

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    basicSsl({
+      /** name of certification */
+      name: "test",
+      /** custom trust domains */
+      domains: ["*.nip.io"],
+
+      certDir: "./.cert",
+    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
